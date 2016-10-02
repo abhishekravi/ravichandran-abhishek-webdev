d2ee7bcf1b6ccb98d2a4267ed6b96d15edccd28a
Abhishek R <abishek.ravichandran@gmail.com>
2016-10-01 20:54:23 -0400

    changed resizing using css

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -2,16 +2,6 @@ $(document).ready(function () {
     //load the website list page in the left pane
     $("#newleftpane").load("website-list.html");
     $("#editleftpane").load("website-list.html");
-    $(window).on("orientationchange", function (event) {
-        //to resize the pane when orientataion changes
-        if (window.innerHeight < window.innerWidth) {
-            $("#newrightpane").addClass("col-xs-6");
-            $("#editrightpane").addClass("col-xs-6");
-        } else {
-            $("#newrightpane").removeClass("col-xs-6");
-            $("#editrightpane").removeClass("col-xs-6");
-        }
-    });
     $("#sortable").sortable();
     $("#sortable").disableSelection();
 });

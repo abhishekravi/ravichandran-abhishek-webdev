96e41455f621a4099037c01fa9a885fee02cf94b
Abhishek R <abishek.ravichandran@gmail.com>
2016-12-05 13:48:48 -0500

    Removed keys

--- project/services/search.service.server.js
+++ project/services/search.service.server.js
@@ -18,16 +18,16 @@ module.exports = function (app) {
         var url = 'http://api.yelp.com/v2/search';
         var params = {
             location: 'Boston',
-            oauth_consumer_key: 'jy6KfFXcDAuhEWaU3yq7Cg', //Consumer Key
-            oauth_token: 'O4AfEjXOKLSgMxYaeE3UTpmiq9pcrFoq', //Token
+            oauth_consumer_key: process.env.YELP_CONSUMER_KEY, //Consumer Key
+            oauth_token: process.env.YELP_TOKEN, //Token
             oauth_signature_method: "HMAC-SHA1",
             oauth_timestamp: n().toString().substr(0,10),
             oauth_nonce: n(),
             category_filter: 'food',
             term: query.text
         };
-        var consumerSecret = 'egCopRSeSLB9j5i0ace_oUbrHBw'; //Consumer Secret
-        var tokenSecret = 'BC-Z3fTHG2_xpUzVl2b-BMZ41pE'; //Token Secret
+        var consumerSecret = process.env.YELP_CONSUMER_SECRET; //Consumer Secret
+        var tokenSecret = process.env.YELP_SECRET_TOKEN; //Token Secret
         var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {encodeSignature: false});
         params['oauth_signature'] = signature;
         var paramURL = qs.stringify(params);
